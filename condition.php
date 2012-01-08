81ee9f9af4225fc51d35ccaa6ed13312ec4c8e96
Yasir Adnan <adnan.628@live.com>
2012-01-08 12:50:06 +0600

    Weather Conditions

---
+++ condition.php
@@ -0,0 +1,145 @@
+<?php
+        
+
+            
+            switch ($cond) 
+            {
+                case "Mostly Cloudy":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/mostly_cloudy.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+               
+                case "Thunderstorm":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/thunderstorm.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Mostly Sunny":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/mostly_sunny.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                case "Sunny":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/sunny.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Rain":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/rain.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Storm":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/storm.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Chance of Rain":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/chance_of_rain.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Partly Cloudy":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/partly_sunny.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                 case "Partly Sunny":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/partly_sunny.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Chance of Storm":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/chance_of_storm.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Cloudy":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/cloudy.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Dust":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/dust.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "flurries":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/flurries.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Fog":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/fog.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Haze":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/haze.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Mist":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/fog.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Rain Snow":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/rain_snow.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Sleet":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/sleet.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Smoke":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/smoke.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+               
+                 case "Snow":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/snow.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                 
+                case "Chance of Snow":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/chance_of_snow.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Light Snow":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/light_snow.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Snow":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/snow.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                case "Clear":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/clear.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Icy":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/icy.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+                
+                case "Light rain":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/light_rain.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+
+                case "Overcast":
+                    $col=get_bloginfo('url').'/wp-content/plugins/weather/images/overcast.png';
+                    echo "<img src=\"$col\" alt=\"\"/>";
+                    break;
+               
+
+
+
+        } 
+
+?>
